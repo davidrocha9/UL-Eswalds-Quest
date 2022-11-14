12c1096bdc877a804f6425640bd6e1f785c4dc52
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2022-11-14 15:16:05 +0100

    FireBall
    Added a fireball attack
    Features
    * Destroys enemy when hits enemy
    * Destroys itself when hits enemy
    * If it will not hit an enemy It will destroy itself within 3 seconds

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField]
+    private int damage = 5;
+    [SerializeField]
+    private float speed = 1.5f; 
+
+    [SerializeField]
+    private EnemyData data;
+
+    private GameObject player;
+    private GameObject enemy;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "FireBall")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Chase();
+    }
+
+    private void Chase()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+
+    
+}
